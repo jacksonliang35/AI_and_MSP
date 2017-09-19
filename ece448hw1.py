eed059d247fd1b6f322be882acd674d5f34f76a7
qinli2 <liqin@wirelessprv-10-195-117-243.near.illinois.edu>
2017-09-19 18:03:17 -0500

    class change

--- ece448hw1.py
+++ ece448hw1.py
@@ -1,7 +1,22 @@
 import os
 
-l = []
-with open("mediumMaze.txt","r", encoding="utf-8") as fp:
-    for line in fp:
-        l.append(line)
-fp.close()
+
+class maze:
+	"""docstring for maze"""
+	def __init__(self, graph=[], height=0, width=0, explored=[], path=[], tree=[]):
+		return
+	def readmaze(self,filename):
+		self.graph= []
+		with open("mediumMaze.txt","r") as fp:
+			for line in fp:
+				self.graph.append(line[0:len(line)-2])
+		fp.close()
+		self.height = len(self.graph)
+		self.width = len(self.graph[0])
+		return
+
+a=maze()
+a.readmaze('mediummaze.txt')
+print(a.graph)
+
+		
