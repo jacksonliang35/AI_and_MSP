fdca6809aae3bb12658c42d53d93e48863e739e7
wuwuwujy <wujiaying211@gmail.com>
2017-10-03 10:15:34 -0500

    comment
    A* and greedy is not commented yet

--- ece448.py
+++ ece448.py
@@ -7,8 +7,9 @@ import copy
 
 class maze:
 	"""docstring for maze"""
+	#We use a stracture to record the basic information of the maze which include the start, end, path, width, height
 	def __init__(self, graph=[], height=0, width=0, explored=[], path=[], tree=[], goalx=[], goaly=[],startx=0,starty=0):
-		self.graph=[]
+		self.graph=[] 
 		self.height=0
 		self.width=0
 		self.explored=[]
@@ -21,6 +22,12 @@ class maze:
 		self.dfscost = -1
 		self.dfspath = []
 		return
+
+	#Input : Maze, txt file containing maze
+	#Output: Maze list
+	#Description: In this function, we read the txt file of maze line by line, 
+	# and store the maze structure in a 2D list. The height and width of maze
+	# are recorded too.
 	def readmaze(self,filename):
 		self.graph= []
 		with open(filename,"r") as fp:
@@ -30,6 +37,12 @@ class maze:
 		self.height = len(self.graph)-1
 		self.width = len(self.graph[0])
 		return
+
+	#Input : Maze
+	#Output: None
+	#Description: In this function, we travel all point of the maze list and find
+	# the x, y poistion of 'P' in the maze. The position of 'P' will be recorded
+	# in startx and starty
 	def findStart(self):
 		for i in range(self.width):
 			for j in range(self.height):
@@ -38,9 +51,18 @@ class maze:
 					self.starty=j
 					break
 		return
+
+	#Input:
+	#Output:
+	#Description:		
 	def heuristic(self, x, y, xg, yg):
 		return abs(y-yg)+abs(x-xg)
 
+	#Input : Maze
+	#Output: None
+	#Description: In this function, we aims to find the target point of maze.
+	# After traveral the 2D maze list, we record the target point as goalx 
+	# and goaly
 	def findGoal(self):
 		for i in range(self.width):
 			for j in range(self.height):
@@ -50,6 +72,11 @@ class maze:
 					break
 		return
 
+	#Input : Maze
+	#Output: x position, y poistion, direction want to travel
+	#Description: This function is used to check if there is a boundary for a given
+	# position we want to travel, if the direction has no block , we return True,
+	# else a Flase warining is returned.
 	def canTravel(self,x, y, dir):
 		if (x < 0) or (y < 0) or (x >= self.width) or (y >= self.height):
 			return False
@@ -65,6 +92,9 @@ class maze:
 		  return (self.graph[y][x+1]!='%')#right
 		return False
 
+	#Input: 
+	#Output:
+	#Description:
 	def greedy(self):
 		counter=0
 		self.path=[]
@@ -110,6 +140,9 @@ class maze:
 			
 		return -1
 
+	#Input:
+	#Output:
+	#Description:
 	def Astar(self):
 		counter=0
 		self.path=[]
@@ -157,25 +190,33 @@ class maze:
 			
 		return -1	
 
+	#Input : maze
+	#Output: path of maze or a false alarm
+	#Description: In this function, we apply BFS algorithm to get the path of a maze
 	def bfs(self):
 		counter =0
 		self.path=[]
-		q = queue.Queue() 
-		discover = dict()
-		self.explored=np.zeros(self.height*self.width)
+		q = queue.Queue() #make a queue to help us record the point that meet first
+		discover = dict() #make a dict help us to do the back tracking
+		self.explored=np.zeros(self.height*self.width) # make a explored map to find if the point is visited
 		self.explored[self.startx+(self.starty)*self.width]=1
-		q.put((self.startx,self.starty))
+		q.put((self.startx,self.starty)) # put the start point to queue
 		while not q.empty():
 			counter+=1
-			current = q.get()
+			current = q.get() #pop a point from queue
 			x=current[0]
 			y=current[1]
+			#if the target position is found, return the path
 			if current[0] == self.goalx[0] and current[1]==self.goaly[0]:
 				while current != (self.startx,self.starty):
 					temp = discover[current[0]+current[1]*self.width]
 					current = (temp%self.width,temp//self.width)
 					self.path.insert(0,current)
 				return counter
+				#for every point we pop from queue, check the four directions,
+				#if we can travel through direction a, enque the point on direction a
+				#mark the current point as visited, and recode the step before to the
+				#discovery dictionary
 			if self.canTravel(x, y, 0):
 				if self.explored[x+(y-1)*self.width]==0:
 					self.explored[x+(y-1)*self.width]=1
@@ -203,39 +244,47 @@ class maze:
 			
 		return -1
 
+	#Input : maze
+	#Output: none
+	#Description: In this function, we read the solution of the maze and 
+	# write it to a txt file
 	def drawsol(self):
 		for i in range(len(self.path)):
 			self.graph[self.path[i][1]][self.path[i][0]]='.'
 		self.graph[self.starty][self.startx]='P'
-		with open('maze_result.txt', 'w') as f:
+		with open('maze_result.txt', 'w') as f:#open a file and write the solution
 			sys.stdout = f
 			for line in self.graph:
 				print(''.join(line))
 		f.close()
 		return
 
+	#Input : maze
+	#Output: stack that containing the path of the maze or false alarm.
+	#Description: In this function, we applied DFS algorithm to the maze
+	# to get the solution
 	def DFS(self):
 		# Use list as stack, find single goal, change maze, return nodes expanded
-		graph = copy.deepcopy(self.graph)  # Is this a reference or a copy?
+		graph = copy.deepcopy(self.graph)  #make a copy to help us marked the visited point of the maze
 		x = self.startx
 		y = self.starty
 		node = 0
 		if x == -1 or y == -1:
 			return -1
-		stack = [(x, y, [])]  # tuple = (x-cood,y-cood,path)
-		while (stack != []):
-			curr = stack.pop()
+		stack = [(x, y, [])] # tuple = (x-cood,y-cood,path)
+		while (stack != []): 
+			curr = stack.pop() # pop a point from the stack and do the following:
 			x = curr[0]
 			y = curr[1]
 			node += 1
-			if graph[x][y] == '.':
+			if graph[x][y] == '.':	# if the targe point is found, return the path 
 				self.dfspath = curr[2]
 				self.dfscost = len(curr[2])
 				return node
-			if graph[x][y] == '*':
+			if graph[x][y] == '*': # if therer is wall at the direction we want to go, do not go
 				continue
 			# Add
-			graph[x][y] = '*'
+			graph[x][y] = '*' # travelsal every direction and push it to the stack
 			if self.canTravel(y, x, 0):
 				stack += [(x - 1, y, curr[2].copy() + [0])]
 			if self.canTravel(y, x, 1):
@@ -244,14 +293,20 @@ class maze:
 				stack += [(x, y - 1, curr[2].copy() + [2])]
 			if self.canTravel(y, x, 3):
 				stack += [(x, y + 1, curr[2].copy() + [3])]
-		return -1
+		return -1	#if no path is found, return a negative 1 alarm
 
+	#Input : maze
+	#Output: none
+	#Description: print the maze on the terminal
 	def printMaze(self):
 		print("printMaze")
 		for line in self.graph:
 			print(''.join(line))
 
-
+	#Input : maze
+	#Output: none
+	#Description: for the path we recorded, use '.' to replace the ' '
+	# in the graph
 	def drawPath(self):
 		path = self.dfspath.copy()
 		curx = self.startx
@@ -271,6 +326,7 @@ class maze:
 				self.graph[curx][cury] = '.'
 		return
 
+#main function
 a=maze()
 a.readmaze('mediummaze.txt')
 
