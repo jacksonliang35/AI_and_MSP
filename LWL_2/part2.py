2ddebafb1a4f97cd65376315302c62b7285d9db0
jacksonliang35 <32082207+jacksonliang35@users.noreply.github.com>
2017-10-25 18:47:24 -0500

    continue working on minimax
    continued....

--- LWL_2/part2.py
+++ LWL_2/part2.py
@@ -4,12 +4,11 @@ import copy
 
 class Board:
     def __init__(self):
-        # Parameters:config, remain, workers
+        # Parameters:config, workers
         config = np.zeros((8,8))   # Empty = 0
         config[0:2] = 2*np.ones((2,8))  # Black = 2
         config[6:8] = np.ones((2,8))    # White = 1
         self.config = config
-        self.remain = np.array([16,16]) # [white,black]
         self.workers = []
         white = []
         black = []
@@ -22,9 +21,9 @@ class Board:
 
     def hasfinished(self):
         # Returns {0, 1, 2} = {not finished, white wins, black wins}
-        if self.remain[0] == 0:
+        if len(self.workers[0]) == 0:
             return 2
-        if self.remain[1] == 0:
+        if len(self.workers[1]) == 0:
             return 1
         for i in range(8):
             if self.config[0,i]==1:
@@ -48,116 +47,41 @@ class Board:
         print()
 
     def canMove(self,pos,dir):
-        # dir = {0,1,2} = {forward left, forward, forward right}
-        # return {0,1,2} = {cannot move, can move no capture, can move with capture}
-        config = self.config
-        if config[pos] == 0:
-            print('Position is empty!')
-            return 0
-        if config[pos] == 1:
-            if dir==0 and pos[0]>0 and pos[1]>0:
-                if config[(pos[0]-1,pos[1]-1)]==0:
-                    return 1
-                elif config[(pos[0]-1,pos[1]-1)]==2:
-                    return 2
-            elif dir==1 and pos[0]>0:
-                if config[(pos[0]-1,pos[1])]==0:
-                    return 1
-                elif config[(pos[0]-1,pos[1])]==2:
-                    return -1
-            elif dir==2 and pos[0]>0 and pos[1]<7:
-                if config[(pos[0]-1,pos[1]+1)]==0:
-                    return 1
-                elif config[(pos[0]-1,pos[1]-1)]==2:
-                    return 2
-        elif config[pos] == 2:
-            if dir==0 and pos[0]<7 and pos[1]<7:
-                if config[(pos[0]+1,pos[1]+1)]==0:
-                    return 1
-                elif config[(pos[0]+1,pos[1]+1)]==2:
-                    return 2
-            elif dir==1 and pos[0]<7:
-                if config[(pos[0]+1,pos[1])]==0:
-                    return 1
-                elif config[(pos[0]+1,pos[1])]==2:
-                    return -1
-            elif dir==2 and pos[0]<7 and pos[1]>0:
-                if config[(pos[0]+1,pos[1]-1)]==0:
-                    return 1
-                elif config[(pos[0]+1,pos[1]-1)]==2:
-                    return 2
-        return 0
+        return canMove(self.config,pos,dir)
 
     def move(self,pos,dir):
-        # NOT check whether can move
-        if self.config[pos] == 0:
-            print('Nothing to Move!')
-            return
-        elif self.config[pos] == 1:
-            if dir==0:
-                if self.config[(pos[0]-1,pos[1]-1)] = 2:
-                    self.remain[1] -= 1
-                    self.workers[1].remove((pos[0]-1,pos[1]-1))
-                self.config[(pos[0]-1,pos[1]-1)] = 1
-                self.workers[0].append((pos[0]-1,pos[1]-1))
-            elif dir==1:
-                self.config[(pos[0]-1,pos[1])] = 1
-                self.workers[0].append((pos[0]-1,pos[1]))
-            elif dir==2:
-                if self.config[(pos[0]-1,pos[1]+1)] = 2:
-                    self.remain[1] -= 1
-                    self.workers[1].remove((pos[0]-1,pos[1]+1))
-                self.workers[0].append((pos[0]-1,pos[1]+1))
-                self.config[(pos[0]-1,pos[1]+1)] = 1
-            self.workers[0].remove(pos)
+        move(self.config,self.workers,pos,dir)
 
-        elif self.config[pos] == 2:
-            if dir==0:
-                if self.config[(pos[0]+1,pos[1]+1)] = 1:
-                    self.remain[0] -= 1
-                    self.workers[0].remove((pos[0]+1,pos[1]+1))
-                self.config[(pos[0]+1,pos[1]+1)] = 2
-                self.workers[1].append((pos[0]+1,pos[1]+1))
-            elif dir==1:
-                self.config[(pos[0]+1,pos[1])] = 2
-                self.workers[1].append((pos[0]+1,pos[1]))
-            elif dir==2:
-                if self.config[(pos[0]+1,pos[1]-1)] = 1:
-                    self.remain[0] -= 1
-                    self.workers[0].remove((pos[0]+1,pos[1]-1))
-                self.config[(pos[0]+1,pos[1]-1)] 	= 2
-                self.workers[1].append((pos[0]+1,pos[1]-1))
-            self.workers[1].remove(pos)
-        self.config[pos] = 0
 
     # Heuristics
     def dh1(self,color):
-        return 2*self.remain[color-1] + random.random()
+        return 2*len(self.workers[color-1]) + random.random()
     def oh1(self,color):
-        return 2*(30-self.remain[2-color]) + random.random()
+        return 2*(30-len(self.workers[2-color])) + random.random()
 
     # Search Strategies
-    def minmax(self,config,workers,depth,color):
+    def minmax(self,config,workers,depth,color,Max):
+        # workers is a deep copy of self.workers
+        # config is a deep copy of self.config
     	if depth==0 or self.hasfinished():
     		return self.oh1(color)
 
-    	if color==0:   # White
+    	if Max:
             strategy = ((-1,-1),-1)
     		value = float('-inf')
-    		for w in self.workers[color-1]:
+    		for w in workers[color-1]:
                 for dir in range(3):
-                    if self.canMove(w,dir):
-                        newboard = copy.deepcopy(board)
-                        newboard.move(w,dir)
+                    if self.canMove(w,dir)==1:
+                        
             			curval=minmax(depth-1,newboard,color,False)
             			if value < curval:
                             value = curval
                             strategy = (w,dir)
     		return strategy
-    	else:  # Black
+    	else:
             strategy = ((-1,-1),-1)
     		value=float('inf')
-    		for w in self.workers[2-color]:
+    		for w in workers[2-color]:
                 for dir in range(3):
                     if board.canMove(w,dir):
                         newboard = copy.deepcopy(board)
@@ -186,7 +110,83 @@ class Board:
     				break
     		return b
 
+def canMove(config,pos,dir):
+    # dir = {0,1,2} = {forward left, forward, forward right}
+    # return {0,1,2} = {cannot move, can move no capture, can move with capture}
+    if config[pos] == 0:
+        print('Position is empty!')
+        return 0
+    if config[pos] == 1:
+        if dir==0 and pos[0]>0 and pos[1]>0:
+            if config[(pos[0]-1,pos[1]-1)]==0:
+                return 1
+            elif config[(pos[0]-1,pos[1]-1)]==2:
+                return 2
+        elif dir==1 and pos[0]>0:
+            if config[(pos[0]-1,pos[1])]==0:
+                return 1
+            elif config[(pos[0]-1,pos[1])]==2:
+                return 0
+        elif dir==2 and pos[0]>0 and pos[1]<7:
+            if config[(pos[0]-1,pos[1]+1)]==0:
+                return 1
+            elif config[(pos[0]-1,pos[1]-1)]==2:
+                return 2
+    elif config[pos] == 2:
+        if dir==0 and pos[0]<7 and pos[1]<7:
+            if config[(pos[0]+1,pos[1]+1)]==0:
+                return 1
+            elif config[(pos[0]+1,pos[1]+1)]==2:
+                return 2
+        elif dir==1 and pos[0]<7:
+            if config[(pos[0]+1,pos[1])]==0:
+                return 1
+            elif config[(pos[0]+1,pos[1])]==2:
+                return 0
+        elif dir==2 and pos[0]<7 and pos[1]>0:
+            if config[(pos[0]+1,pos[1]-1)]==0:
+                return 1
+            elif config[(pos[0]+1,pos[1]-1)]==2:
+                return 2
+    return 0
 
+def move(config,workers,pos,dir):
+    # NOT check whether can move
+    if self.config[pos] == 0:
+        print('Nothing to Move!')
+        return
+    elif self.config[pos] == 1:
+        if dir==0:
+            if self.config[(pos[0]-1,pos[1]-1)] = 2:
+                self.workers[1].remove((pos[0]-1,pos[1]-1))
+            self.config[(pos[0]-1,pos[1]-1)] = 1
+            self.workers[0].append((pos[0]-1,pos[1]-1))
+        elif dir==1:
+            self.config[(pos[0]-1,pos[1])] = 1
+            self.workers[0].append((pos[0]-1,pos[1]))
+        elif dir==2:
+            if self.config[(pos[0]-1,pos[1]+1)] = 2:
+                self.workers[1].remove((pos[0]-1,pos[1]+1))
+            self.workers[0].append((pos[0]-1,pos[1]+1))
+            self.config[(pos[0]-1,pos[1]+1)] = 1
+        self.workers[0].remove(pos)
+
+    elif self.config[pos] == 2:
+        if dir==0:
+            if self.config[(pos[0]+1,pos[1]+1)] = 1:
+                self.workers[0].remove((pos[0]+1,pos[1]+1))
+            self.config[(pos[0]+1,pos[1]+1)] = 2
+            self.workers[1].append((pos[0]+1,pos[1]+1))
+        elif dir==1:
+            self.config[(pos[0]+1,pos[1])] = 2
+            self.workers[1].append((pos[0]+1,pos[1]))
+        elif dir==2:
+            if self.config[(pos[0]+1,pos[1]-1)] = 1:
+                self.workers[0].remove((pos[0]+1,pos[1]-1))
+            self.config[(pos[0]+1,pos[1]-1)] 	= 2
+            self.workers[1].append((pos[0]+1,pos[1]-1))
+        self.workers[1].remove(pos)
+    self.config[pos] = 0
 
 
 
