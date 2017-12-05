1092712619ef782945cd267bf8c162eec8bc2e21
Yuchen Liang <yuchenliang@wirelessprv-10-195-124-185.near.illinois.edu>
2017-12-04 19:07:51 -0600

    q learning w/ some bugs

--- LWL_4/part2_Qin_liang.py
+++ LWL_4/part2_Qin_liang.py
@@ -1,3 +1,4 @@
+import numpy as np
 import random
 import math # math.floor
 import matplotlib.pyplot as plt
@@ -22,7 +23,10 @@ class Pongcontstate:
         return
 
     def getCurrentFrame(self):
-        return plt.plot([self.bx],[1-self.by],'ro',[1,1],[0.8-self.py,1-self.py],linewidth=5.0)
+        fig = plt.figure()
+        plt.plot([self.bx],[1-self.by],'ro',[1,1],[0.8-self.py,1-self.py],linewidth=5.0)
+        plt.show()
+        return
 
     def getDiscreteState(self):
         return Pongdiscstate(self.bx,self.by,self.vx,self.vy,self.py,self.reward)
@@ -33,7 +37,7 @@ class Pongcontstate:
         return False
 
     def nextstate(self,action):
-        # action = {-1,0,1}
+        # action = {-1,0,1} = {up,stay,down}
         end = False
         new_bx = self.bx + self.vx
         new_by = self.by + self.vy
@@ -52,7 +56,7 @@ class Pongcontstate:
             new_by = 2-new_by
             new_vy = -new_vy
         if new_bx > 1:
-            # Check whether crossing panel
+            # Check whether bouncing onto panel
             cross = self.by + (1-self.bx)*self.vy/self.vx
             if cross < self.py or cross > self.py+0.2:
                 end = True
@@ -76,7 +80,6 @@ class Pongdiscstate:
             self.vx = -1
             self.vy = -1
             self.py = -1
-            self.reward = -1
         else:
             # discretize ball position
             if bx >= 1:
@@ -104,29 +107,64 @@ class Pongdiscstate:
                 self.py = 11
             else:
                 self.py = math.floor(12 * py / 0.8)
-            return
-            # reward
-            self.reward = rd
+        return
+
+def getindex(state):
+    # type(state) = Pongdiscstate
+    # action = {-1,0,1}
+    if state.hasfinished():
+        return 12*12*2*3*12    # bx*by*vx*vy*py
+    else:
+        raw_ind = [state.bx,state.by,(state.vx+1)//2,(state.vy+1),state.py]
+        size = [12,12,2,3,12]
+        ind = raw_ind[0]
+        for i in range(1,5):
+            ind = ind * size[i] + raw_ind[i]
+        return ind
 
 if __name__ == '__main__':
     # Train
-    N = 1000
+    C = 1
+    gamma = 0.9
+    N = 100
+    Q = np.zeros((12*12*2*3*12+1,3))    # Action-utility
+    N = np.zeros((12*12*2*3*12+1,3))    # State-action frequency
     for i in range(N):
-
+        # Play game
+        curr = Pongcontstate()
+        curd = curr.getDiscreteState()
+        ind = getindex(curd)
+        while not curr.hasfinished():
+            a = np.argmax(Q[ind,:])-1   # {-1,0,1}
+            # Get next state
+            nexts = curr.nextstate(a)
+            nextd = nexts.getDiscreteState()
+            nextind = getindex(nextd)
+            # Get current learning rate
+            alpha = C/(C+N[ind,a+1])
+            # TD update
+            Q[ind,a+1] = Q[ind,a+1] + alpha*(curr.rd + gamma*max(Q[nextind,:]) - Q[ind,a+1])
+            N[ind,a+1] = N[ind,a+1] + 1
+            # Re-assign curr
+            curr = nexts
+            curd = nextd
+            ind = nextind
+        if i % 10 == 0:
+            print(i)
     #############################################
     # Test & Display
     curr = Pongcontstate()
     play = [curr]
     while not curr.hasfinished():
-        a = ...
+        curd = curr.getDiscreteState()
+        a = np.argmax(Q[getindex(curd),:])-1
         curr = curr.nextstate(a)
         play.append(curr)
-
     # Draw solution
     fig = plt.figure()
     ims = []
     for s in play:
-        ims.append(plt.plot([s.bx],[1-s.by],'ro',[1,1],[0.8-s.py,1-s.py],linewidth=5.0))
+        ims.append(plt.plot([s.bx],[1-s.by],'ro',[1,1],[0.8-s.py,1-s.py],linewidth=6.0))
     im_ani = anim.ArtistAnimation(fig, ims, interval=50, repeat_delay=3000, blit=True)
     plt.axis([0,1,0,1])
     plt.show()
